e4edc94acfe6b63828283024b506550216a87b6f
Owen <owenwwild@Gmail.com>
2026-02-13 22:36:40 -0500

    Fixed duration

--- backend/data/FlightSearchResult.py
+++ backend/data/FlightSearchResult.py
@@ -14,6 +14,7 @@ class FlightSearchResult(BaseModel):
     airline: str
     price: float
     segments: List[FlightSegment]
+    duration_min : int 
 
 
 class FlightSearchResultByCombination(BaseModel):
